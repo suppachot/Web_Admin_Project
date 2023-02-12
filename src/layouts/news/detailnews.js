43feee9e9e92ab0929154d07b93a72d80bb7d8d2
suppachot <train2544@gmail.com>
2023-02-12 13:26:26 +0700

    news CRUC Backend_pass

--- src/layouts/news/detailnews.js
+++ src/layouts/news/detailnews.js
@@ -13,8 +13,48 @@ import { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
 
-function DetailNews(){
+function DetailNews() {
 
+    const { NewsNo } = useParams();
+    const [newsdata, newsdatachange] = useState(null);
+
+    useEffect(() => {
+        fetch("http://localhost:5000/news/" + NewsNo).then((res) => {
+            return res.json();
+        }).then((resp) => {
+            newsdatachange(resp);
+        }).catch((err) => {
+            console.log(err.message);
+        })
+    }, []);
+    
+
+    return (
+        <DashboardLayout>
+            <DashboardNavbar />
+            <div className="container">
+
+                <div className="card row" style={{ "textAlign": "left" }}>
+                    <div className="card-title">
+                        <h2>News Detail</h2>
+                    </div>
+                    <div className="card-body"></div>
+                    {newsdata &&
+                        <div>
+                            <h2>TopicNews : <b>{newsdata.TopicNews}</b> ({newsdata.NewsNo},{newsdata.NewsDate})</h2>
+                            <h4>Detail list</h4>
+                            <h6>Detail : {newsdata.NewsDetail}</h6>
+                            <h6>Email is : {newsdata.CreateBy}</h6>
+                            <h6>UpdateDate : {newsdata.UpdateDate}</h6>
+                            <h6>UpdateBy : {newsdata.UpdateBy}</h6>
+                            <Link className="btn btn-danger" to="/news" >Back to Listing</Link>
+                        </div>
+                    }
+                </div>
+            </div>
+
+        </DashboardLayout>
+    );
 }
 
 export default DetailNews;
--- src/layouts/news/news.js
+++ src/layouts/news/news.js
@@ -11,12 +11,30 @@ import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
 import DashboardNavbar from "examples/Navbars/DashboardNavbar";
 import { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { Axios } from "axios";
 
 function News() {
 
   const [newsdata, newsdatachange] = useState(null);
+  const navigate = useNavigate();
 
-
+  const LoadDetail = (NewsNo) => {
+    navigate("/detailsenews" + NewsNo);
+  }
+  const LoadEdit = (NewsNo) => {
+    navigate("/editnews" + NewsNo);
+  }
+  const Removefunction = (NewsNo) => {
+    if (window.confirm('Do you want to remove?')) {
+      Axios.post("http://localhost:5000/deletenews" + NewsNo, {
+      }).then((res) => {
+        alert('Removed successfully.')
+        window.location.reload();
+      }).catch((err) => {
+        console.log(err.message)
+      })
+    }
+  }
   useEffect(() => {
     fetch("http://localhost:5000/news").then((res) => {
       return res.json();
@@ -26,39 +44,25 @@ function News() {
       console.log(err.message);
     })
   }, [])
-  /*
-    useEffect(() => {
-      fetch("http://localhost:5000/news")
-          .then(res => res.json())
-          .then(
-              (result) => {
-                  setIsLoaded(true);
-                  setItems(result);
-              },
-              (error) => {
-                  setIsLoaded(true);
-                  setError(error);
-              }
-          )
-  }, [])
-  */
+
   return (
     <DashboardLayout>
       <DashboardNavbar />
       <div className="LayoutContainer">
         <div className="card">
-          <div className="card-title ">
+          <div className="card-title text-center">
             <h2>News</h2>
           </div>
           <div className="card-body">
             <div className="btn">
-              <Link to="/news/create" className="btn btn-success">Add New</Link>
+              <Link to="/createnews" className="btn btn-success">Add New</Link>
             </div>
             <table className="table table-bordered">
-              <thead className="bg-dark text-white">
+              <thead className="bg-dark text-white text-center">
                 <tr>
                   <td>NewsNo</td>
                   <td>NewsDate</td>
+                  <td>TopicNews</td>
                   <td>NewsDetail</td>
                   <td>CreateBy</td>
                   <td>UpdateDate</td>
@@ -67,16 +71,16 @@ function News() {
                 </tr>
               </thead>
               <tbody>
-                {newsdata &&
-                  newsdata.map(item => (
+                {newsdata &&newsdata.map(item => (
                     <tr key={item.NewsNo}>
                       <td>{item.NewsNo}</td>
                       <td>{item.NewsDate}</td>
+                      <td>{item.TopicNews}</td>
                       <td>{item.NewsDetail}</td>
                       <td>{item.CreateBy}</td>
                       <td>{item.UpdateDate}</td>
                       <td>{item.UpdateBy}</td>
-                      <td><a onClick={() => { LoadEdit(item.NewsNo) }} className="btn btn-success">Edit</a>
+                      <td><a onClick={() => {LoadEdit(item.NewsNo) }} className="btn btn-success">Edit</a>
                         <a onClick={() => { Removefunction(item.NewsNo) }} className="btn btn-danger">Remove</a>
                         <a onClick={() => { LoadDetail(item.NewsNo) }} className="btn btn-primary">Details</a>
                       </td>
