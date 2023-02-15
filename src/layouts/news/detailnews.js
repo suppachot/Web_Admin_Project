5af69c0880269e6c1e3d6485a89075a68f51f86a
suppachot <train2544@gmail.com>
2023-02-16 01:49:18 +0700

    detail

--- src/layouts/news/detailnews.js
+++ src/layouts/news/detailnews.js
@@ -10,7 +10,7 @@ import MDTypography from "components/MDTypography";
 import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
 import DashboardNavbar from "examples/Navbars/DashboardNavbar";
 import { useEffect, useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 
 function DetailNews() {
@@ -19,36 +19,40 @@ function DetailNews() {
     const [newsdata, newsdatachange] = useState(null);
 
     useEffect(() => {
-        fetch("http://localhost:5000/news/" + NewsNo).then((res) => {
-            
-        }).then((resp) => {
-            newsdatachange(resp);
-        }).catch((err) => {
-            console.log(err.message);
-        })
+        fetch("http://localhost:5000/news/detail/" + NewsNo)
+            .then(res => res.json())
+            .then((resp) => {
+                newsdatachange(resp);
+            }).catch((err) => {
+                console.log(err.message);
+            })
     }, []);
-    
+
 
     return (
         <DashboardLayout>
             <DashboardNavbar />
+
             <div className="container">
 
-                <div className="card row" style={{ "textAlign": "left" }}>
+                <div className="card row mb-3" style={{ "textAlign": "center" }}>
                     <div className="card-title">
                         <h2>News Detail</h2>
                     </div>
                     <div className="card-body"></div>
-                    {newsdata &&
+                    {newsdata && newsdata.map(val => (
                         <div>
-                            <h2>TopicNews : <b>{newsdata.TopicNews}</b> ({newsdata.NewsNo},{newsdata.NewsDate})</h2>
+                            <h2>TopicNews : <b>{val.TopicNews}</b> ({val.NewsNo},{val.NewsDate})</h2>
                             <h4>Detail list</h4>
-                            <h6>Detail : {newsdata.NewsDetail}</h6>
-                            <h6>Email is : {newsdata.CreateBy}</h6>
-                            <h6>UpdateDate : {newsdata.UpdateDate}</h6>
-                            <h6>UpdateBy : {newsdata.UpdateBy}</h6>
+                            <h6>Detail : {val.NewsDetail}</h6>
+                            <h6>Email is : {val.CreateBy}</h6>
+                            <h6>UpdateDate : {val.UpdateDate}</h6>
+                            <h6>UpdateBy : {val.UpdateBy}</h6>
+                            <br></br>
                             <Link className="btn btn-danger" to="/news" >Back to Listing</Link>
+                            
                         </div>
+                    ))
                     }
                 </div>
             </div>
